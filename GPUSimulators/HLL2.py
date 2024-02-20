64d446cbbfa4f07acd291a37736c6ef927dd33cc
Hicham Agueny <95568317+HichamAgueny@users.noreply.github.com>
2024-02-20 15:35:01 +0100

    add hip_check

--- GPUSimulators/HLL2.py
+++ GPUSimulators/HLL2.py
@@ -50,6 +50,20 @@ class HLL2 (Simulator.BaseSimulator):
     dt: Size of each timestep (90 s)
     g: Gravitational accelleration (9.81 m/s^2)
     """
+    def hip_check(call_result):
+        err = call_result[0]
+        result = call_result[1:]
+        if len(result) == 1:
+            result = result[0]
+        if isinstance(err, hip.hipError_t) and err != hip.hipError_t.hipSuccess:
+            raise RuntimeError(str(err))
+        elif (
+            isinstance(err, hiprtc.hiprtcResult)
+            and err != hiprtc.hiprtcResult.HIPRTC_SUCCESS
+        ):
+            raise RuntimeError(str(err))
+        return result
+    
     def __init__(self, 
                  context, 
                  h0, hu0, hv0, 
